bd6ebe8aabbade318e588e425252bddc7a1fea58
LuizFelipeGatto <luizfelipegatto08@gmail.com>
2022-05-23 20:19:44 -0300

    Terminando projeto

--- app/src/main/java/br/edu/ifsuldeminas/combustivel/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/combustivel/MainActivity.java
@@ -1,10 +1,16 @@
 package br.edu.ifsuldeminas.combustivel;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         textInputEditTextGas = findViewById(R.id.imputEditTextGasValueId);
         buttonCalculate = findViewById(R.id.buttonCalculateId);
         imageViewResult = findViewById(R.id.imageView2);
-        imageViewShare = findViewById(R.id.imageView3);
+        imageViewShare = findViewById(R.id.imageShare);
         textViewTip = findViewById(R.id.textView3);
     }
 
@@ -72,5 +78,38 @@ public class MainActivity extends AppCompatActivity {
         imageViewShare.setVisibility(View.VISIBLE);
         textViewTip.setText(betterOption);
         textViewTip.setVisibility(View.VISIBLE);
+    }
+
+
+    public void imageShare(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.alert_title);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View alertDialogView = inflater.inflate(R.layout.alert_dialog_gas_station_view,null);
+        builder.setView(alertDialogView);
+
+        builder.setNegativeButton(R.string.alert_negative_button, null);
+        builder.setPositiveButton(R.string.alert_positive_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                EditText editText = alertDialogView.findViewById(R.id.editTextAlertDialogGasStationId);
+                String gasStation = editText.getText().toString();
+
+                String message = "Preços no posto %s. Etanol %.2f - Gasolina %.2f. %s, relação %.0f%s.";
+                message = String.format(message, gasStation, ethanolValue, gasValue, betterOption, (ethanolValue / gasValue)*100, "%");
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+            }
+        });
+
+        builder.create().show();
+
     }
 }
